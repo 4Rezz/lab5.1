b3e6f9b0972dd502d658beb0a224d359886e0ee5
Roman Ilyshun <romanilyshun@gmail.com>
2020-12-03 10:33:28 +0200

    lab5.1

---
+++ lab5.1/lab5.1.cpp
@@ -0,0 +1,25 @@
+﻿// Lab_04_3.cpp
+// < Ілюшин Роман >
+// Лабораторна робота No 5.1
+// Функції що містять арифметичний вираз
+// Варіант 0.2
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+double k(const double x, const double y); // прототип
+int main()
+{
+	double p, q;
+
+		cout << "p = "; cin >> p;
+	cout << "q = "; cin >> q;
+
+	double c = k(p + sqrt(q), q - sqrt(p)) - k(1, p + q) * k(p + sqrt(q), q - sqrt(p)) - k(1, p + q);
+	cout << "c = " << c << endl;
+	return 0;
+}
+double k(const double x, const double y) // визначення
+{
+	return (x / (abs(pow(x, 3) + pow(y, 3)))) + (y / (abs(x + y)));
+}
